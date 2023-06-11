270ced103d4adedd0c36c16201211e5e88fc90b1
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-06-11 09:24:32 -0400

    feat: exercise console input

---
+++ ApplicationArguments/ConsoleInput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationArguments
+{
+    internal class ConsoleInput
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your name: ");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
