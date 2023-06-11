13f751c4fcb46f4332a5689df6bd11e0b54af84a
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-06-11 12:48:40 -0400

    feat: operators

---
+++ ApplicationArguments/Operators/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Arithmetic operators");
+Console.WriteLine();
+Console.WriteLine("Increment and decrement");
+int a = 7;
+int b = a++;
+Console.WriteLine($"a: {a}, b: {b}");
+
+int c = 7;
+int d = --c;
+Console.WriteLine($"c: {c}, d: {d}");
+
+Console.WriteLine("Comparison operators");
+int k = 17;
+int l = 15;
+Console.WriteLine(k > l);
+Console.WriteLine(k < l);
+Console.WriteLine(k >= l);
+Console.WriteLine(k <= l);
+
+Console.WriteLine("Boolean logical operators");
+
+bool isRegistered = false;
+Console.WriteLine($"isRegisterd: {isRegistered}");
+Console.WriteLine($"isRegisterd NOT: {!isRegistered}");
+
+bool isWednesday = true;
+bool isWeekend = false;
+Console.WriteLine(isWednesday && isWeekend);
+Console.WriteLine(isWednesday || isWeekend);
+
+Console.WriteLine("Equality operators");
+string myName = "Anzhelika";
+string myCountry = "USA";
+int myLuckyNumber = 16;
+
+Console.WriteLine(myLuckyNumber != 5);
+Console.WriteLine(myLuckyNumber == 16);
+Console.WriteLine(myName == myCountry);
+
