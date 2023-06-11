cac34bdc88de4325b4e3c4e965d714204ed0dda7
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-06-11 11:58:48 -0400

    feat: casting and value type conversion

---
+++ ApplicationArguments/CastingAndValueTypeConversions/Program.cs
@@ -0,0 +1,16 @@
+﻿int age = 31;
+float weight = 78.7f;
+
+Console.WriteLine($"age: {age}, weight: {weight}");
+
+//implicit conversation
+long myAge = age;
+
+//explicit conversion
+int lessWeight = (int)weight; //precission loss
+int notMyAge = (int)myAge; //no precission loss
+
+long bigNumber = 1111111111111111;
+int smallerType = (int)bigNumber;
+
+Console.WriteLine(smallerType);
