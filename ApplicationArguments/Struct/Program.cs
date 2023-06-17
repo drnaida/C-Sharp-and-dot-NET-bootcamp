9dcaeb29ad02b13733209997b9a07fb16859cac1
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-06-17 04:50:05 -0400

    feat: structs

---
+++ ApplicationArguments/Struct/Program.cs
@@ -0,0 +1,25 @@
+﻿var rectangle = new Rectangle(200, 300);
+
+public struct Rectangle
+{
+    public Rectangle (double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public double Width { get; set; }
+    public double Height { get; set; }
+}
+
+public readonly struct ImmutableRectangle
+{
+    public ImmutableRectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public double Width { get; init; }
+    public double Height { get; init; }
+}
