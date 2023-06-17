8f97cea8f45259102036f3db3b88284f153d2b47
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-06-17 03:23:12 -0400

    feat: inheritance

--- ApplicationArguments/Inheritance/Program.cs
+++ ApplicationArguments/Inheritance/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿Person person1 = new Person("James");
+Employee employee2 = new Employee("Jessica", "ceo");
+
+Console.WriteLine($"{person1.Name}: {person1.GetId()}");
+Console.WriteLine($"{employee2.Name}: {employee2.GetId()}");
+
+public interface ISalary
+{
+    decimal GetSalary();
+}
+public class Person
+{
+    public string Name { get; set; }
+
+    public Person (string name)
+    {
+        Name = name;
+    }
+
+    public virtual string GetId()
+    {
+        return Name.ToLower();
+    }
+}
+
+public class Employee: Person, ISalary
+{
+    public string Role { get; set; }
+    public Employee(string name, string role) : base (name)
+    {
+        Role = role;
+    }
+
+    public override string GetId()
+    {
+        return $"{Role.ToLower()}-{base.GetId()}";
+    }
+
+    public decimal GetSalary()
+    {
+        return 2000;
+     }
+}
