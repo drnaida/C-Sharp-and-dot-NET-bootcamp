20df4aa0c47bc91b2b0190088d711f21cfaf94e8
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-06-17 03:36:08 -0400

    feat: advanced variables scope

---
+++ ApplicationArguments/AdvancedVariableScope/Program.cs
@@ -0,0 +1,25 @@
+﻿Book book1 = new Book("Harry Potter and the Sorcerer's Stone", "J. K. Rowling");
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+
+    public void PrintSummary()
+    {
+        string summary = $"{Title} - {Author}";
+        Console.WriteLine(summary);
+    }
+
+    public int GetTitleLenghtWithoutWhiteSpaces()
+    {
+        string titleWithoutWhiteSpaces = Title.Replace(" ", "");
+        return titleWithoutWhiteSpaces.Length;
+    }
+}
