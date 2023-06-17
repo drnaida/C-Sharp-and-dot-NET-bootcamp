6727608fd15a84c66345970a440a5fce14b40e8b
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-06-17 03:28:43 -0400

    feat: po;ymorphism

---
+++ ApplicationArguments/polymorphism/Program.cs
@@ -0,0 +1,31 @@
+﻿Person person = new Person();
+Person employee = new Employee();
+Person customer = new Customer();
+
+person.Greeting();
+employee.Greeting();
+customer.Greeting();
+
+public class Person
+{
+    public virtual void Greeting()
+    {
+        Console.WriteLine("Hi, I am a person.");
+    }
+}
+
+public class Employee : Person
+{
+    public override void Greeting()
+    {
+        Console.WriteLine("Hi, I am an employee");    
+    }
+}
+
+public class Customer : Person
+{
+    public override void Greeting()
+    {
+        Console.WriteLine("Hi, I am a customer");
+    }
+}
