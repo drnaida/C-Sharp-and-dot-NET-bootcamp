a8f61c12d59447336c737491e5dbb669d53b3eed
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-06-17 03:44:22 -0400

    feat: constant

---
+++ ApplicationArguments/ConstKeyword/Program.cs
@@ -0,0 +1,10 @@
+﻿const int age = 32;
+//age = 24;
+
+public class Person
+{
+    const int Age = 21;
+    const string ProductName = "C# course";
+    const string Version = "2";
+    const string FullProductName = $"{ProductName} {Version}";
+}
