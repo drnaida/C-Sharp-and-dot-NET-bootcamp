10bce9e8e71bd10fcfd36bd74b8026cd20860081
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-06-19 02:54:21 -0400

    feat: filtering data using linq

---
+++ ApplicationArguments/FilteringDataUsingLINQ/Program.cs
@@ -0,0 +1,36 @@
+﻿var numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6 };
+
+//deferred query execution with anonymous method
+var evenNumbers = numbers.Where(x => x % 2 == 0);
+
+//deferred query execution
+var evenNumbers2 = numbers.Where(FilterEvenNumbers);
+
+//if we add toList() or anything else with to, it will make the query
+//execute right now and not include 7
+//immediate query execution
+var evenNumbers3 = numbers.Where(FilterEvenNumbers).ToList();
+
+//still will not see the 7 in evenNumbers and evenNumbers2
+numbers.Add(7);
+numbers.Add(8);
+
+foreach (int num in evenNumbers)
+{
+    Console.Write("{0, 1}", num);
+}
+Console.WriteLine();
+foreach (int num in evenNumbers2)
+{
+    Console.Write("{0, 1}", num);
+}
+Console.WriteLine();
+foreach (int num in evenNumbers3)
+{
+    Console.Write("{0, 1}", num);
+}
+
+static bool FilterEvenNumbers(int num)
+{
+    return num % 2 == 0;
+}
