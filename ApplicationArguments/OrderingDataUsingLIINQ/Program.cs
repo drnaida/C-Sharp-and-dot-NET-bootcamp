c1d9bfb99fbdd8787bfc09518f1e0cc3dfd03867
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-06-19 03:06:11 -0400

    feat: ordering data using ling

---
+++ ApplicationArguments/OrderingDataUsingLIINQ/Program.cs
@@ -0,0 +1,32 @@
+﻿var animals = new List<string> { "Deer", "Sheep", "Cat", "Dog", "Bull", "Donkey", "Goat" };
+
+foreach (var animal in animals.OrderBy(animal => animal)) {
+    Console.WriteLine(animal);
+}
+Console.WriteLine();
+
+foreach (var animal in animals.OrderByDescending(animal => animal))
+{
+    Console.WriteLine(animal);
+}
+
+var persons = new List<Person>
+{
+    new Person("John", "Deer"),
+    new Person("John", "Apple"),
+    new Person("Tim", "Walters"),
+    new Person("April", "Johnson"),
+};
+
+foreach (var person in persons.OrderBy(person => person.FirstName).ThenBy(person => person.Lastname))
+{
+    Console.WriteLine($"{person.FirstName} {person.Lastname}");
+};
+
+animals.Reverse();
+foreach (var animal in animals)
+{
+    Console.WriteLine(animal);
+}
+
+record Person(string FirstName, string Lastname);
