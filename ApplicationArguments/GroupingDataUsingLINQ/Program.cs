19b87e5d7ac5edc321c225167a1eed2293b08c99
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-06-19 10:32:51 -0400

    Grouping using LINQ

---
+++ ApplicationArguments/GroupingDataUsingLINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿var employees = new List<Employee>
+{
+    new Employee("John", "Miller", "Development"),
+    new Employee("Jack", "Stevens", "Sales"),
+    new Employee("Maria", "Mc Allister", "Sales"),
+    new Employee("Christopher", "Collum", "Project Management"),
+    new Employee("Javier", "Alegro", "Development"),
+};
+
+var personsByDeparment = employees.GroupBy(person => person.Department);
+
+foreach (var department in personsByDeparment)
+{
+    Console.WriteLine($"Department: {department.Key}");
+
+    foreach (var employee in department)
+    {
+        Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+    }
+}
+record Employee(string FirstName, string LastName, string Department);
