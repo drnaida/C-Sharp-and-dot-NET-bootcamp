c06e8fd1a36a05afe8d7a348d8c16e50a55d0c0f
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-07-02 15:09:52 -0400

    feat: GenericList

---
+++ ApplicationArguments/GenericList/Program.cs
@@ -0,0 +1,21 @@
+﻿var numbers = new List<int>();
+var day = 22;
+numbers.Add(day);
+
+foreach  (var number in numbers)
+{
+    Console.WriteLine($"Number {number}");
+}
+
+var persons = new List<Person>();
+var person1 = new Person("Andrew");
+persons.Add(person1);
+
+public class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; set; }
+}
