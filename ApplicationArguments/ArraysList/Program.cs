31bce6d9c5e1f014f026efeae8450f4d38946341
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-07-02 15:05:25 -0400

    feat: ArrayLust

---
+++ ApplicationArguments/ArraysList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+var arrayList = new ArrayList();
+
+var day = 32;
+arrayList.Add(day);
+
+var person1 = new Person("Andrew");
+arrayList.Add(person1);
+
+var firstElement = arrayList[0];
+Console.WriteLine($"First element: {firstElement}");
+
+foreach (var item in arrayList )
+{
+    if (item is Person) {
+        var person = item as Person;
+        if (person != null)
+        {
+            Console.WriteLine(person.Name);
+        }
+        else
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+public class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
+}
