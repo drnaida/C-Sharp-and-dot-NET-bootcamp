7035ddebd97a87ff4088a5805265bd4427bef414
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2023-07-02 14:59:08 -0400

    feat: arrays

---
+++ ApplicationArguments/Arrays/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] array1 = new int[4];
+
+array1[0] = 7;
+array1[1] = 16;
+array1[2] = 21;
+array1[3] = 27;
+
+Console.WriteLine(array1);
+Console.WriteLine(array1[0]);
+
+//Array initilizer
+int[] array2 = new int[] {7, 16, 21, 27};
+
+Console.WriteLine(array2);
+Console.WriteLine(array2[0]);
+
+//Alternative Syntax
+int[] array3 = {7, 16, 21, 27};
+Console.WriteLine(array3);
+Console.WriteLine(array3[0]);
+
+//Uninitialized elements
+int[] array4 = new int[5];
+Console.WriteLine(array4[0]);
+
+Person[] persons = new Person[2];
+persons[0] = new Person("John");
+persons[1] = new Person("Jenna");
+Console.WriteLine(persons);
+var person = persons[0];
+Console.WriteLine(person.Name);
+
+foreach(Person person2 in persons)
+{
+    Console.WriteLine(person2.Name);
+}
+
+public class Person
+{
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public string Name { get; set;}
+}
